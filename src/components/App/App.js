85c8264bd3f361065f9c4f0356152b9cb124d3d8
Rex <proandceo@gmail.com>
2023-03-07 08:44:04 +1000

    update Pass state as props to the Board

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,7 +24,7 @@ class App extends Component {
           playing={this.state.playing}
           numTiles={this.state.numTiles}
         />
-        <Board />
+        <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
       </div>
     );
   }
