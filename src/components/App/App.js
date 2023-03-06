7b85773a79b73aea651844e313378048017d3363
Rex <proandceo@gmail.com>
2023-03-07 08:31:25 +1000

    Instantiate the state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
-import OptionsPanel from '../OptionsPanel'
-import Board from '../Board'
+import React, { Component } from "react";
+import OptionsPanel from "../OptionsPanel";
+import Board from "../Board";
 
-import './App.css';
+import "./App.css";
 
-class App extends Component{
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numTiles: 36,
+      playing: false,
+      previousTileIndex: null,
+      tiles: [],
+      toBeCleared: null,
+    };
+  }
 
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
+    return (
+      <div className="App">
+        <header className="App-header">Turbo-Matcher</header>
         <OptionsPanel />
-        <Board />
-      }
-    </div>
-  );
-
+        <Board />}
+      </div>
+    );
   }
 }
 
