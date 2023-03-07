7b21d8761bef104d21d180786f400b19bd88e570
Rex <proandceo@gmail.com>
2023-03-07 15:32:20 +1000

    update

---
+++ refactor/GameContext.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+createContext = () => {
+  numTiles: 36;
+  playing: false;
+  handleNumTileChange: () => {};
+  startPlaying: () => {};
+};
+
+const GameContext = () => {};
+
+export default GameContext;
---
+++ refactor/components/TileSelector/TileSelector.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./TileSelector.css";
+import useHover from "../../hooks";
+
+const TileSelector = (props) => {
+  const [ref, hovered] = useHover();
+
+  const dropdown = hovered ? (
+    <div className="tileSelectorContent">
+      <div
+        className="number"
+        onClick={() => {
+          props.handleNumTileChange(4);
+        }}
+      >
+        4
+      </div>
+      <div
+        className="number"
+        onClick={() => {
+          props.handleNumTileChange(16);
+        }}
+      >
+        16
+      </div>
+      <div
+        className="number"
+        onClick={() => {
+          props.handleNumTileChange(36);
+        }}
+      >
+        36
+      </div>
+    </div>
+  ) : null;
+
+  return (
+    <div className="tileSelector">
+      <div>Number of Tiles</div>
+      <div className="tileSelectorDropdown" ref={ref}>
+        {props.numTiles}
+        {dropdown}
+      </div>
+    </div>
+  );
+};
+
+export default TileSelector;
