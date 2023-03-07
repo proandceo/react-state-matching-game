ea030cfee7a3f126850bbb2923d4db0ccdc40eec
Rex <proandceo@gmail.com>
2023-03-07 15:04:48 +1000

    Update hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -7,11 +7,19 @@ const useHover = () => {
   const enter = () => {
     setHovered(true);
   };
+
+  const leave = () => {
+    setHovered(true);
+  };
+
   useEffect(() => {
     const refCopy = ref;
     refCopy.current.addEventListener("mouseenter", enter);
-    refCopy.current.addEventListener("mouseleave", () => {});
-    return () => {};
+    refCopy.current.addEventListener("mouseleave", leave);
+    return () => {
+      ref.refCopy.current.removeEventListener("mouseenter", enter);
+      ref.refCopy.current.removeEventListener("mouseleave", leave);
+    };
   });
 
   return [ref, hovered];
