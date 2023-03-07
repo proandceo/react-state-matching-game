4228dacfa6c7dcc74b18d2a0db037bbb300ee0e9
Rex <proandceo@gmail.com>
2023-03-07 15:46:43 +1000

    Update Button.js

--- refactor/components/Button/Button.js
+++ refactor/components/Button/Button.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Button.css";
+import GameContext from "../../GameContext";
 
 const Button = (props) => (
-  <button onClick={props.startGame}>{props.playing ? "reset" : "start"}</button>
+  <GameContext.Consumer>
+    {({ playing, startGame }) => (
+      <button onClick={props.startGame}>
+        {props.playing ? "reset" : "start"}
+      </button>
+    )}
+  </GameContext.Consumer>
 );
 
 export default Button;
