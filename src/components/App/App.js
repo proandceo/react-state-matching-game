c3d4a409397cca5fcaef2ce9f78764273a44bcb8
Rex <proandceo@gmail.com>
2023-03-07 12:55:23 +1000

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -33,6 +33,7 @@ class App extends Component {
         <OptionsPanel
           playing={this.state.playing}
           numTiles={this.state.numTiles}
+          startGame={this.props.startGame}
         />
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
       </div>
