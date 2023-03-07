d1bd1733b66114e1db48d647468febf0f36cbc49
Rex <proandceo@gmail.com>
2023-03-07 15:59:53 +1000

    Update TileSelector.js

--- refactor/components/TileSelector/TileSelector.js
+++ refactor/components/TileSelector/TileSelector.js
@@ -11,37 +11,23 @@ const TileSelector = (props) => {
       {({ numTiles, handleNumTileChange }) => {
         const dropdown = hovered ? (
           <div className="tileSelectorContent">
-            <div
-              className="number"
-              onClick={() => {
-                props.handleNumTileChange(4);
-              }}
-            >
+            <div className="number" onClick={() => handleNumTileChange(4)}>
               4
             </div>
-            <div
-              className="number"
-              onClick={() => {
-                props.handleNumTileChange(16);
-              }}
-            >
+            <div className="number" onClick={() => handleNumTileChange(16)}>
               16
             </div>
-            <div
-              className="number"
-              onClick={() => {
-                props.handleNumTileChange(36);
-              }}
-            >
+            <div className="number" onClick={() => handleNumTileChange(36)}>
               36
             </div>
           </div>
         ) : null;
+
         return (
           <div className="tileSelector">
             <div>Number of Tiles</div>
             <div className="tileSelectorDropdown" ref={ref}>
-              {props.numTiles}
+              {numTiles}
               {dropdown}
             </div>
           </div>
