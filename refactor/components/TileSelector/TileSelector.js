0d35c487c023791f08a4391291958d750994a2c0
Rex <proandceo@gmail.com>
2023-03-07 15:57:07 +1000

    Update TileSelector.js

--- refactor/components/TileSelector/TileSelector.js
+++ refactor/components/TileSelector/TileSelector.js
@@ -1,47 +1,53 @@
 import React from "react";
 import "./TileSelector.css";
 import useHover from "../../hooks";
+import GameContext from "../../GameContext";
 
 const TileSelector = (props) => {
   const [ref, hovered] = useHover();
 
-  const dropdown = hovered ? (
-    <div className="tileSelectorContent">
-      <div
-        className="number"
-        onClick={() => {
-          props.handleNumTileChange(4);
-        }}
-      >
-        4
-      </div>
-      <div
-        className="number"
-        onClick={() => {
-          props.handleNumTileChange(16);
-        }}
-      >
-        16
-      </div>
-      <div
-        className="number"
-        onClick={() => {
-          props.handleNumTileChange(36);
-        }}
-      >
-        36
-      </div>
-    </div>
-  ) : null;
-
   return (
-    <div className="tileSelector">
-      <div>Number of Tiles</div>
-      <div className="tileSelectorDropdown" ref={ref}>
-        {props.numTiles}
-        {dropdown}
-      </div>
-    </div>
+    <GameContext.Consumer>
+      {({ numTiles, handleNumTileChange }) => {
+        const dropdown = hovered ? (
+          <div className="tileSelectorContent">
+            <div
+              className="number"
+              onClick={() => {
+                props.handleNumTileChange(4);
+              }}
+            >
+              4
+            </div>
+            <div
+              className="number"
+              onClick={() => {
+                props.handleNumTileChange(16);
+              }}
+            >
+              16
+            </div>
+            <div
+              className="number"
+              onClick={() => {
+                props.handleNumTileChange(36);
+              }}
+            >
+              36
+            </div>
+          </div>
+        ) : null;
+        return (
+          <div className="tileSelector">
+            <div>Number of Tiles</div>
+            <div className="tileSelectorDropdown" ref={ref}>
+              {props.numTiles}
+              {dropdown}
+            </div>
+          </div>
+        );
+      }}
+    </GameContext.Consumer>
   );
 };
 
